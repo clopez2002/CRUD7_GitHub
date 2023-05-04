c97e9119740307a0ec9635310db032e2dfaa25f0
clopez2002 <morgan_pirata2003@yahoo.com.ar>
2023-05-04 13:51:40 -0300

    Controller creado, pafina que muestra lista de runners creada

---
+++ src/main/java/pildoras/es/controlador/Controller.java
@@ -0,0 +1,29 @@
+package pildoras.es.controlador;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.stereotype.Controller
+@RequestMapping("runnersPage")
+public class Controller {
+
+/*****************************************************************************/
+
+    @RequestMapping("/runnersListPage")
+    public String showRunnersListMethod (Model theModel){
+
+        return "runners_list_file";
+    }
+
+/*****************************************************************************/
+
+/*****************************************************************************/
+
+/*****************************************************************************/
+
+/*****************************************************************************/
+
+
+
+
+}
